e328f8895a242bb4310b6940c8125a78a91f33dd
Kim <howtoad123@gmail.com>
2022-05-23 11:05:37 +0200

    full set up

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import Header from "../templates/Header";
+
+const About = () => {
+  return (
+    <>
+      <Header heading="I told you politely" text="This is your last warning" />
+      <img src="https://media.istockphoto.com/vectors/grumpy-old-man-vector-id164510786"></img>
+    </>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import Header from "../templates/Header";
+
+const Home = () => {
+  return <Header heading="Not Welcome" text="Please leave immediately" />;
+};
+
+export default Home;
